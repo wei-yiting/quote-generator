7e70cb004ce54f199726228b01c89976a0ee7122
WYT <tina.yiting.wei@gmail.com>
2021-03-12 16:09:31 +0800

    added a loader to the page, avoiding blank page before fetch api finish its work

--- script.js
+++ script.js
@@ -4,22 +4,37 @@
 let apiQuotes = [];
 
 // DOM selection
+const quoteContainer = document.querySelector('#quote-container');
 const quoteText = document.querySelector('#quote-content');
 const authorText = document.querySelector('#author')
 const fbShareBtn = document.querySelector('#facebook');
 const newQuoteBtn = document.querySelector('#new-quote');
+const loader = document.querySelector('#loader');
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+
+}
+
+// Hide Loading
+function completeLoading() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 // ===== Show New Quote ======
 
 function newQuote() {
+    loading();
     // pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // check if author field is blank and replace it with unkown
     if (!quote.author) {
         authorText.innerText = 'Unknown';
     } else {
-        authorText.innerText = quote.author;
+        authorText.innerText = `- ${quote.author}`;
     }
     // check quote length and determine font-size
     if (quote.text.length > 80) {
@@ -27,20 +42,23 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote')
     }
+    // Set Quote, Hide Loader
     quoteText.innerText = quote.text;
+    completeLoading();
 }
 
 
 // ====== get quote from API ======
 
 async function getQuote() {
+    loading();
     const apiURL = 'https://type.fit/api/quotes'
     try {
         const response = await fetch(apiURL);
         apiQuotes = await response.json();
         return newQuote();
     } catch (error) {
-        // catch error
+        // catch error Here
     }
 }
 
@@ -48,8 +66,10 @@ async function getQuote() {
 // initiate first quote
 getQuote();
 
-newQuoteBtn.addEventListener('click', newQuote);
 
+// event listener
+newQuoteBtn.addEventListener('click', newQuote);
+fbShareBtn.addEventListener('click', shareFaceBook);
 
 
 //  ====== share quote to facebook =====
@@ -71,5 +91,3 @@ const shareFaceBook = function () {
         quote: `${quoteText.innerText} \n- ${authorText.innerText}`,
     }, function (response) { });
 };
-
-fbShareBtn.addEventListener('click', shareFaceBook);
